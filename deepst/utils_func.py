7333efad67dfa650c89535b30dc56a550842b6f3
spatial-Transcriptomics <93078228+spatial-Transcriptomics@users.noreply.github.com>
2022-07-22 13:53:00 +0800

    Update utils_func.py

--- deepst/utils_func.py
+++ deepst/utils_func.py
@@ -270,3 +270,27 @@ def read_stereoSeq(path,
     adata.uns["spatial"][library_id]["scalefactors"]["spot_diameter_fullres"] = spot_diameter_fullres
 
     return adata
+  
+  
+def refine(sample_id, pred, dis, shape="hexagon"):
+    refined_pred=[]
+    pred=pd.DataFrame({"pred": pred}, index=sample_id)
+    dis_df=pd.DataFrame(dis, index=sample_id, columns=sample_id)
+    if shape=="hexagon":
+        num_nbs=6 
+    elif shape=="square":
+        num_nbs=4
+    else:
+        print("Shape not recongized, shape='hexagon' for Visium data, 'square' for ST data.")
+    for i in range(len(sample_id)):
+        index=sample_id[i]
+        dis_tmp=dis_df.loc[index, :].sort_values()
+        nbs=dis_tmp[0:num_nbs+1]
+        nbs_pred=pred.loc[nbs.index, "pred"]
+        self_pred=pred.loc[index, "pred"]
+        v_c=nbs_pred.value_counts()
+        if (v_c.loc[self_pred]<num_nbs/2) and (np.max(v_c)>num_nbs/2):
+            refined_pred.append(v_c.idxmax())
+        else:           
+            refined_pred.append(self_pred)
+    return refined_pred
